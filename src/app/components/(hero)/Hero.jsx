db821af477ed6751cde72df3fda85a5981afaab9
nanajulieaugustine <naje0004@stud.kea.dk>
2025-03-28 15:32:46 +0100

    det meste dynamisk

--- src/app/components/(hero)/Hero.jsx
+++ src/app/components/(hero)/Hero.jsx
@@ -1,12 +1,16 @@
-"use strict";
+"use client";
 import HeroText from "./Herotext";
 import Heroimg from "./Heroimg";
+import { useState } from "react";
 const Hero = () => {
+  const [chosenWatch, setWatchClicked] = useState(false);
   return (
-    <section>
-      <HeroText></HeroText>
-      <Heroimg></Heroimg>
-    </section>
+    !chosenWatch && (
+      <section>
+        <HeroText></HeroText>
+        <Heroimg chosenWatch={chosenWatch} setWatchClicked={setWatchClicked} />
+      </section>
+    )
   );
 };
 
--- src/app/components/(hero)/Heroimg.jsx
+++ src/app/components/(hero)/Heroimg.jsx
@@ -1,12 +1,44 @@
-"use strict";
+"use client";
+import { useState } from "react";
+import Image from "next/image";
 const Heroimg = () => {
+  const [chosenWatch, setWatchClicked] = useState("/images/black_watch.png");
   return (
     <div>
-      <img src="../images/black_watch.png" alt="" />
+      <div className="overviewWatch">
+        <Image src={chosenWatch} width={500} height={500} alt="watch" />
+        <div className="colorContainer">
+          <div
+            className={
+              chosenWatch == "/black_watch.png" ? "active black" : "black"
+            }
+          ></div>
+          <div className="blue"></div>
+          <div className="pink"></div>
+        </div>
+      </div>
       <div className="displayWatches">
-        <img src="../images/black_watch.png" alt="" />
-        <img src="../images/blue_watch.png" alt="" />
-        <img src="../images/pink_watch.png" alt="" />
+        <Image
+          width={500}
+          height={500}
+          alt="watch"
+          onClick={() => setWatchClicked("/images/black_watch.png")}
+          src="/images/black_watch.png"
+        />
+        <Image
+          width={500}
+          height={500}
+          alt="watch"
+          onClick={() => setWatchClicked("/images/blue_watch.png")}
+          src="/images/blue_watch.png"
+        />
+        <Image
+          width={500}
+          height={500}
+          alt="watch"
+          onClick={() => setWatchClicked("/images/pink_watch.png")}
+          src="/images/pink_watch.png"
+        />
       </div>
     </div>
   );
